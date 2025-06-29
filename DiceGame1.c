784ce295d3e7ec96a64cc352aa4d44428d39841f
Yuki Takahashi <s1320167@u-aizu.ac.jp>
2025-06-29 16:34:54 +0900

    Dice game: initial revision

---
+++ DiceGame1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int GetRandom(int min, int max);
+int main(){
+    int ran,total = 0;
+    printf("Rolling dice...\n");
+    for(int i=1; i<=2; i++){
+        ran = GetRandom(1,6);
+        printf("Die %d: %d\n", i, ran);
+        total += ran;
+    }
+    printf("Total value: %d\n",total);
+    return 0;
+}
+int GetRandom(int min, int max){
+    return min + (int)(rand() * (max - min + 1.0) / (1.0 + RAND_MAX));
+}
